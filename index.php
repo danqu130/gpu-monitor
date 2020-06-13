78971c018925060d406b0288f3c8d7f5b197a365
danquxunhuan130 <danquxunhuan130@gmail.com>
2020-06-13 19:38:01 +0800

    add js file due to local network

--- index.php
+++ index.php
@@ -623,7 +623,7 @@ if (!isset($_GET["content"])) { ?>
 
     </div>
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="js/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script type="text/javascript">
     function preparePage () {
